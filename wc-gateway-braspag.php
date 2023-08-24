8b03ba0ef1c0a1b1d847f077d507aac2e8d9d345
Willian Rodrigues <willian.rodrigues@devellop.com.br>
2023-08-24 19:20:12 -0300

    ident && clean

--- wc-gateway-braspag.php
+++ wc-gateway-braspag.php
@@ -15,7 +15,7 @@
  *
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 
@@ -26,35 +26,38 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @return string
  */
-function wc_braspag_missing_wc_notice() {
+function wc_braspag_missing_wc_notice()
+{
 	/* translators: 1. URL link. */
-	echo '<div class="error"><p><strong>' . sprintf( esc_html__( 'Braspag requires WooCommerce to be installed and active. You can download %s here.', 'woocommerce-braspag' ), '<a href="https://woocommerce.com/" target="_blank">WooCommerce</a>' ) . '</strong></p></div>';
+	echo '<div class="error"><p><strong>' . sprintf(esc_html__('Braspag requires WooCommerce to be installed and active. You can download %s here.', 'woocommerce-braspag'), '<a href="https://woocommerce.com/" target="_blank">WooCommerce</a>') . '</strong></p></div>';
 }
 
-add_action( 'plugins_loaded', 'woocommerce_gateway_braspag_init' );
+add_action('plugins_loaded', 'woocommerce_gateway_braspag_init');
 
-function woocommerce_gateway_braspag_init() {
-	load_plugin_textdomain( 'woocommerce-braspag', false, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
+function woocommerce_gateway_braspag_init()
+{
+	load_plugin_textdomain('woocommerce-braspag', false, plugin_basename(dirname(__FILE__)) . '/languages');
 
-	if ( ! class_exists( 'WooCommerce' ) ) {
-		add_action( 'admin_notices', 'wc_braspag_missing_wc_notice' );
+	if (!class_exists('WooCommerce')) {
+		add_action('admin_notices', 'wc_braspag_missing_wc_notice');
 		return;
 	}
 
-	if ( ! class_exists( 'WC_Braspag' ) ) :
+	if (!class_exists('WC_Braspag')):
 		/**
 		 * Required minimums and constants
 		 */
-		define( 'WC_BRASPAG_VERSION', '2.3.30' );
-		define( 'WC_BRASPAG_WP_VERSION', $wp_version );
-		define( 'WC_BRASPAG_MIN_PHP_VER', '5.6.0' );
-		define( 'WC_BRASPAG_MIN_WC_VER', '4.0.0' );
-		define( 'WC_BRASPAG_MIN_WP_VER', '5.3.2' );
-		define( 'WC_BRASPAG_MAIN_FILE', __FILE__ );
-		define( 'WC_BRASPAG_PLUGIN_URL', untrailingslashit( plugins_url( basename( plugin_dir_path( __FILE__ ) ), basename( __FILE__ ) ) ) );
-		define( 'WC_BRASPAG_PLUGIN_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
+		define('WC_BRASPAG_VERSION', '2.3.30');
+		define('WC_BRASPAG_WP_VERSION', $wp_version);
+		define('WC_BRASPAG_MIN_PHP_VER', '5.6.0');
+		define('WC_BRASPAG_MIN_WC_VER', '4.0.0');
+		define('WC_BRASPAG_MIN_WP_VER', '5.3.2');
+		define('WC_BRASPAG_MAIN_FILE', __FILE__);
+		define('WC_BRASPAG_PLUGIN_URL', untrailingslashit(plugins_url(basename(plugin_dir_path(__FILE__)), basename(__FILE__))));
+		define('WC_BRASPAG_PLUGIN_PATH', untrailingslashit(plugin_dir_path(__FILE__)));
 
-		class WC_Braspag {
+		class WC_Braspag
+		{
 
 			/**
 			 * @var Singleton The reference the *Singleton* instance of this class
@@ -66,8 +69,9 @@ function woocommerce_gateway_braspag_init() {
 			 *
 			 * @return Singleton The *Singleton* instance.
 			 */
-			public static function get_instance() {
-				if ( null === self::$instance ) {
+			public static function get_instance()
+			{
+				if (null === self::$instance) {
 					self::$instance = new self();
 				}
 				return self::$instance;
@@ -79,7 +83,9 @@ function woocommerce_gateway_braspag_init() {
 			 *
 			 * @return void
 			 */
-			private function __clone() {}
+			private function __clone()
+			{
+			}
 
 			/**
 			 * Private unserialize method to prevent unserializing of the *Singleton*
@@ -87,14 +93,17 @@ function woocommerce_gateway_braspag_init() {
 			 *
 			 * @return void
 			 */
-			private function __wakeup() {}
+			private function __wakeup()
+			{
+			}
 
 			/**
 			 * Protected constructor to prevent creating a new instance of the
 			 * *Singleton* via the `new` operator from outside of this class.
 			 */
-			private function __construct() {
-				add_action( 'admin_init', array( $this, 'install' ) );
+			private function __construct()
+			{
+				add_action('admin_init', array($this, 'install'));
 				$this->init();
 			}
 
@@ -104,17 +113,18 @@ function woocommerce_gateway_braspag_init() {
 			 * @since 1.0.0
 			 * @version 1.0.0
 			 */
-			public function init() {
-				if ( is_admin() ) {
-					require_once dirname( __FILE__ ) . '/includes/admin/class-wc-braspag-privacy.php';
-					require_once dirname( __FILE__ ) . '/includes/admin/class-wc-braspag-customer-seller-attributes.php';
+			public function init()
+			{
+				if (is_admin()) {
+					require_once dirname(__FILE__) . '/includes/admin/class-wc-braspag-privacy.php';
+					require_once dirname(__FILE__) . '/includes/admin/class-wc-braspag-customer-seller-attributes.php';
 				}
 
-				require_once dirname( __FILE__ ) . '/includes/class-wc-braspag-exception.php';
-				require_once dirname( __FILE__ ) . '/includes/class-wc-braspag-logger.php';
-				require_once dirname( __FILE__ ) . '/includes/class-wc-braspag-helper.php';
-				include_once dirname( __FILE__ ) . '/includes/class-wc-braspag-payment-tokens.php';
-				include_once dirname( __FILE__ ) . '/includes/class-wc-braspag-pagador-api.php';
+				require_once dirname(__FILE__) . '/includes/class-wc-braspag-exception.php';
+				require_once dirname(__FILE__) . '/includes/class-wc-braspag-logger.php';
+				require_once dirname(__FILE__) . '/includes/class-wc-braspag-helper.php';
+				include_once dirname(__FILE__) . '/includes/class-wc-braspag-payment-tokens.php';
+				include_once dirname(__FILE__) . '/includes/class-wc-braspag-pagador-api.php';
 				include_once dirname(__FILE__) . '/includes/class-wc-braspag-risk-api.php';
 				include_once dirname(__FILE__) . '/includes/class-wc-braspag-oauth-api.php';
 				include_once dirname(__FILE__) . '/includes/class-wc-braspag-mpi-api.php';
@@ -135,14 +145,14 @@ function woocommerce_gateway_braspag_init() {
 					require_once dirname(__FILE__) . '/includes/admin/class-wc-braspag-admin-notices.php';
 				}
 
-				add_filter( 'woocommerce_payment_gateways', array( $this, 'add_gateways' ) );
-				add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( $this, 'plugin_action_links' ) );
+				add_filter('woocommerce_payment_gateways', array($this, 'add_gateways'));
+				add_filter('plugin_action_links_' . plugin_basename(__FILE__), array($this, 'plugin_action_links'));
 
 				// Modify emails.
 				// add_filter( 'woocommerce_email_classes', array( $this, 'add_emails' ), 20 );
 
-				if ( version_compare( WC_VERSION, '3.4', '<' ) ) {
-					add_filter( 'woocommerce_get_sections_checkout', array( $this, 'filter_gateway_order_admin' ) );
+				if (version_compare(WC_VERSION, '3.4', '<')) {
+					add_filter('woocommerce_get_sections_checkout', array($this, 'filter_gateway_order_admin'));
 				}
 			}
 
@@ -152,9 +162,10 @@ function woocommerce_gateway_braspag_init() {
 			 * @since 1.0.0
 			 * @version 1.0.0
 			 */
-			public function update_plugin_version() {
-				delete_option( 'woocommerce_braspag_version' );
-				update_option( 'woocommerce_braspag_version', WC_BRASPAG_VERSION );
+			public function update_plugin_version()
+			{
+				delete_option('woocommerce_braspag_version');
+				update_option('woocommerce_braspag_version', WC_BRASPAG_VERSION);
 			}
 
 			/**
@@ -163,16 +174,17 @@ function woocommerce_gateway_braspag_init() {
 			 * @since 1.0.0
 			 * @version 1.0.0
 			 */
-			public function install() {
-				if ( ! is_plugin_active( plugin_basename( __FILE__ ) ) ) {
+			public function install()
+			{
+				if (!is_plugin_active(plugin_basename(__FILE__))) {
 					return;
 				}
 
-				if ( ! defined( 'IFRAME_REQUEST' ) && ( WC_BRASPAG_VERSION !== get_option( 'woocommerce_braspag_version' ) ) ) {
-					do_action( 'wc_braspag_updated' );
+				if (!defined('IFRAME_REQUEST') && (WC_BRASPAG_VERSION !== get_option('woocommerce_braspag_version'))) {
+					do_action('wc_braspag_updated');
 
-					if ( ! defined( 'WC_BRASPAG_INSTALLING' ) ) {
-						define( 'WC_BRASPAG_INSTALLING', true );
+					if (!defined('WC_BRASPAG_INSTALLING')) {
+						define('WC_BRASPAG_INSTALLING', true);
 					}
 
 					$this->update_plugin_version();
@@ -185,11 +197,12 @@ function woocommerce_gateway_braspag_init() {
 			 * @since 1.0.0
 			 * @version 1.0.0
 			 */
-			public function plugin_action_links( $links ) {
+			public function plugin_action_links($links)
+			{
 				$plugin_links = array(
-					'<a href="admin.php?page=wc-settings&tab=checkout&section=braspag">' . esc_html__( 'Settings', 'woocommerce-braspag' ) . '</a>',
+					'<a href="admin.php?page=wc-settings&tab=checkout&section=braspag">' . esc_html__('Settings', 'woocommerce-braspag') . '</a>',
 				);
-				return array_merge( $plugin_links, $links );
+				return array_merge($plugin_links, $links);
 			}
 
 			/**
@@ -198,9 +211,10 @@ function woocommerce_gateway_braspag_init() {
 			 * @since 1.0.0
 			 * @version 1.0.0
 			 */
-			public function add_gateways( $methods ) {
+			public function add_gateways($methods)
+			{
 
-                $methods[] = 'WC_Gateway_Braspag';
+				$methods[] = 'WC_Gateway_Braspag';
 				$methods[] = 'WC_Gateway_Braspag_CreditCard';
 				$methods[] = 'WC_Gateway_Braspag_CreditCard_JustClick';
 				$methods[] = 'WC_Gateway_Braspag_DebitCard';
