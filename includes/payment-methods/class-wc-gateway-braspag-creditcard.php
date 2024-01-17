9eff2dd7e947992f1f61ce5cb1e837991851d0a8
Willian Rodrigues <devellop.smart.solutions@gmail.com>
2024-01-17 18:56:22 -0300

    Modificações de Marcação de Iniciação da Compra     Adicionar validação de bandeira master     Adicionar validação de cartão salvo     Adicionar JSON no nó de pedido

--- includes/payment-methods/class-wc-gateway-braspag-creditcard.php
+++ includes/payment-methods/class-wc-gateway-braspag-creditcard.php
@@ -606,14 +606,20 @@ class WC_Gateway_Braspag_CreditCard extends WC_Gateway_Braspag
         }
 
         $customer_wants_to_save_card = $checkout->get_value('wc-braspag_creditcard-new-payment-method') == 'true';
+        $brandCard = $checkout->get_value('braspag_creditcard-card-type');
 
         $card_data = [
             "CardNumber" => str_replace(" ", "", $checkout->get_value('braspag_creditcard-card-number')),
             "Holder" => $checkout->get_value('braspag_creditcard-card-holder'),
             "ExpirationDate" => $card_expiration_date,
             "SecurityCode" => $checkout->get_value('braspag_creditcard-card-cvc'),
-            "Brand" => $checkout->get_value('braspag_creditcard-card-type'),
+            "Brand" => $brandCard,
             "SaveCard" => $this->save_card == 'yes' && $customer_wants_to_save_card
+        ];
+
+        $InitiatedTransactionIndicator = [
+            "Category" => "C1",
+            "Subcategory" => "CredentialsOnFile"
         ];
 
         $provider = $this
@@ -621,6 +627,10 @@ class WC_Gateway_Braspag_CreditCard extends WC_Gateway_Braspag
 
         if (isset($this->soft_descriptor) && !empty($this->soft_descriptor)) {
             $payment_data['SoftDescriptor'] = $this->soft_descriptor;
+        }
+
+        if ($this->save_card == 'yes' && $customer_wants_to_save_card && $brandCard == 'Master') {
+            $payment_data['InitiatedTransactionIndicator'] = $InitiatedTransactionIndicator;
         }
 
         $payment_data = array_merge($payment_data, [
@@ -636,7 +646,6 @@ class WC_Gateway_Braspag_CreditCard extends WC_Gateway_Braspag
             "Recurrent" => false,
             "DoSplit" => false,
             "CreditCard" => $card_data
-            //"ExtraDataCollection" => $this->extra_data_collection
         ]);
 
         return apply_filters(
