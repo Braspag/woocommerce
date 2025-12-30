731259c6d65e1991a551aa979d0ae90cff782e64
Willian Rodrigues <williangringo@outlook.com>
2025-12-30 10:13:15 -0300

    fix: update validation when failuretype is empty or null

--- includes/payment-methods/class-wc-gateway-braspag-debitcard.php
+++ includes/payment-methods/class-wc-gateway-braspag-debitcard.php
@@ -303,6 +303,10 @@ class WC_Gateway_Braspag_DebitCard extends WC_Gateway_Braspag
         $checkout = WC()->checkout();
         $failureType = (string) $checkout->get_value('bpmpi_auth_failure_type');
 
+        if ($failureType === '' || $failureType === '0') {
+			return;
+		}
+
         $message = __('Debit Card Payment Failure.', 'woocommerce-braspag');
         $appendMpi = false;
 
